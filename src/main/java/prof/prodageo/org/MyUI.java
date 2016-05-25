6ca89e720f42c3133aa36499e69461eb5b28ac61
Pierrick Randriamanantsoa <pierrick.randriamanantsoa@insa-rouen.fr>
2016-05-26 00:04:13 +0200

    Changement de cas d'utilisation

--- src/main/java/prof/prodageo/org/MyUI.java
+++ src/main/java/prof/prodageo/org/MyUI.java
@@ -2,6 +2,7 @@ package prof.prodageo.org;
 
 import javax.servlet.annotation.WebServlet;
 
+import java.io.Serializable;
 import com.vaadin.annotations.Theme;
 import com.vaadin.server.ThemeResource;
 import com.vaadin.annotations.VaadinServletConfiguration;
@@ -17,7 +18,7 @@ import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.*;
 import com.vaadin.ui.themes.ValoTheme;
 import com.vaadin.shared.ui.label.ContentMode;
-
+import com.vaadin.ui.AbstractSelect.ItemCaptionMode;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -37,97 +38,93 @@ import com.vaadin.ui.Button.ClickEvent;
 @Widgetset("prof.prodageo.org.MyAppWidgetset")
 public class MyUI extends UI {
 
-        private static final Logger log = LoggerFactory.getLogger(MyUIServlet.class);
+private static final Logger log = LoggerFactory.getLogger(MyUIServlet.class);
 
-    /* explicit declaration as attributes of graphical components for GenMyModel */
-        final VerticalLayout popupContent = new VerticalLayout();
-        final VerticalLayout layoutPrincipal = new VerticalLayout();
-        final GridLayout layout1 = new GridLayout(3,1);
-        final Label titre = new Label("<h1>Friendly Fish</h1>", ContentMode.HTML);
-        final PopupView popup = new PopupView("Mon compte Utilisateur", popupContent);
-        final Button  connecter = new Button("Se connecter");
-        final HorizontalLayout layout2 = new HorizontalLayout();
-        final TextField barreRecherche = new TextField();
-        final Button boutonRecherche = new Button("Recherche") ;
-        final Button boutonRechercheAvancee = new Button("Recherche avancée") ;
-        final HorizontalLayout layout3 = new HorizontalLayout();
-        final Button boutonComparateur = new Button("<br>Comparateur</br> <br><b>Poisson/Poisson</b></br> <br></br>") ;
-        final Button boutonListePoissons = new Button("<br>Rechercher</br> <br>la <b>liste des poissons compatibles</br> <br>avec le poisson</b></br>") ;
-        final Button boutonEau = new Button("<br>Rechercher</br> <br>la liste des poissons compatibles</br> <br>avec un <b>type d'eau</b></br>") ;
+/* explicit declaration as attributes of graphical components for GenMyModel */
+final VerticalLayout layoutPrincipal = new VerticalLayout();
+
+final GridLayout layout1 = new GridLayout(3,1);
+final Label titreComparateur = new Label("Compatibilité <b>Poisson/Poisson</b>", ContentMode.HTML);
+final Label titre = new Label("<h1>Friendly Fish</h1>", ContentMode.HTML);
+final VerticalLayout popupContent = new VerticalLayout();
+final PopupView popup = new PopupView("Mon compte Utilisateur", popupContent);
+final Button connecter = new Button("Se connecter");
+
+final GridLayout layout2 = new GridLayout(5,1);
+final VerticalLayout layout3 = new VerticalLayout();
+final Button boutonListePoissons = new Button("<br>Rechercher</br> <br>la <b>liste des poissons compatibles</br> <br>avec le poisson</b></br>");
+final Button boutonEau = new Button("<br>Rechercher</br> <br>la liste des poissons compatibles</br> <br>avec un <b>type d'eau</b></br>");
+
+final VerticalLayout layout4 = new VerticalLayout();
+final ComboBox choixPoisson = new ComboBox("Poisson 1");
+final Button boutonAjouter = new Button("Ajouter dans l'aquarium");
+final Label description = new Label("Description du poisson 1");
+final Label compatibilite = new Label("Caractéristiques du poisson 1");
+
+final VerticalLayout layout5 = new VerticalLayout();
+
+final VerticalLayout layout6 = new VerticalLayout();
+
+final VerticalLayout layout7 = new VerticalLayout();
+final ComboBox choixPoisson2 = new ComboBox("Poisson 2");
+final Button boutonAjouter2 = new Button("Ajouter dans l'aquarium");
+final Label description2 = new Label("Description du poisson 2");
+final Label compatibilite2 = new Label("Caractéristiques du poisson 2");
+
+/* explicit callback */
+/* https://vaadin.com/docs/-/part/framework/application/application-events.html */
 
 
+public class IdPoisson extends Object implements Serializable {
+String nomPoisson;
+IdPoisson (String nom) {
+        nomPoisson = nom;
+}
 
-    /* explicit callback */
-    /* https://vaadin.com/docs/-/part/framework/application/application-events.html */
+public String toString () {
+        return nomPoisson;
+}
+}
 
-    /*public class ClickMeClass implements Button.ClickListener
-    {
-        public void buttonClick(ClickEvent event)
-        {
-            layoutPrincipal.addComponent(new Label("Thanks " + name1.getValue() + ", it works!"));
-            log.info("Button clicked with value : " + name1.getValue());
+@Override
+protected void init(VaadinRequest vaadinRequest) {
+
+        String ensemblePoissons[] = {"Truite", "Saumon", "Poisson Rouge", "Étoile de Mer"};
+        for (int i=0; i<ensemblePoissons.length; i++) {
+                choixPoisson.addItem(new IdPoisson(ensemblePoissons[i]));
+                choixPoisson2.addItem(new IdPoisson(ensemblePoissons[i]));
         }
-    }*/
 
-
-
-    @Override
-    protected void init(VaadinRequest vaadinRequest) {
-
-        // final VerticalLayout layout = new VerticalLayout();
-
-        // final TextField name = new TextField();
-
-
-        /*
-        Button button = new Button("Click Me");
-        button.addClickListener( e -> {
-            layout.addComponent(new Label("Thanks " + name.getValue()
-                    + ", it works!"));
-            log.info("Button clicked with value : " + name.getValue());
-        });
-        */
-
-        /*ClickMeClass callback = new ClickMeClass() ;
-        button2.addClickListener( callback ) ;
-        ClickMeClass callback = new ClickMeClass() ;
-        button3.addClickListener( callback ) ;*/
+        boutonEau.addClickListener( e ->
+        layoutPrincipal.addComponent(new Label("Le bouton Eau marche")));
+        boutonListePoissons.addClickListener( e ->
+        layoutPrincipal.addComponent(new Label("Le bouton Liste Poissons marche")));
+        boutonAjouter.addClickListener( e ->
+        layoutPrincipal.addComponent(new Label("Le bouton Ajouter1 marche")));
+        boutonAjouter2.addClickListener( e ->
+        layoutPrincipal.addComponent(new Label("Le bouton Ajouter2 marche")));
 
         titre.setWidth("100%");
-        popup.setIcon(FontAwesome.USER);
-
+        titre.setSizeUndefined();
         layout1.addComponent(titre,1,0);
-        layout1.addComponent(popup,2,0);
+        layout1.setComponentAlignment(titre, Alignment.MIDDLE_CENTER);
+
+        titreComparateur.setIcon(FontAwesome.HEART_O);
+        titreComparateur.addStyleName(ValoTheme.BUTTON_ICON_ALIGN_TOP);
+        layout1.addComponent(titreComparateur,0,0);
 
         popupContent.addComponent(new TextField("Login"));
         popupContent.addComponent(new TextField("Mot de passe"));
         popupContent.addComponent(connecter);
+        popup.setSizeUndefined();
+        layout1.addComponent(popup,2,0);
+        layout1.setComponentAlignment(popup, Alignment.MIDDLE_CENTER);
 
         layout1.setSizeFull();
-        layout1.setMargin(true);
+        //layout1.setMargin(true);
         layout1.setSpacing(true);
         setContent(layout1);
 
-        titre.setSizeUndefined();
-        popup.setSizeUndefined();
-        layout1.setComponentAlignment(titre, Alignment.MIDDLE_CENTER);
-        layout1.setComponentAlignment(popup, Alignment.MIDDLE_CENTER);
-
-        barreRecherche.setIcon(FontAwesome.SEARCH);
-        barreRecherche.setInputPrompt("Recherchez votre poisson");
-        barreRecherche.addStyleName(ValoTheme.TEXTFIELD_INLINE_ICON);
-        barreRecherche.setColumns(50);
-        boutonRechercheAvancee.setIcon(FontAwesome.SEARCH_PLUS);
-        boutonRechercheAvancee.addStyleName(ValoTheme.TEXTFIELD_INLINE_ICON);
-
-        layout2.addComponents(barreRecherche, boutonRecherche, boutonRechercheAvancee);
-        layout2.setMargin(true);
-        layout2.setSpacing(true);
-        setContent(layout2);
-
-        boutonComparateur.setHtmlContentAllowed(true);
-        boutonComparateur.setIcon(FontAwesome.HEART_O);
-        boutonComparateur.addStyleName(ValoTheme.BUTTON_ICON_ALIGN_TOP);
         boutonListePoissons.setHtmlContentAllowed(true);
         boutonListePoissons.setIcon(FontAwesome.LIST);
         boutonListePoissons.addStyleName(ValoTheme.BUTTON_ICON_ALIGN_TOP);
@@ -135,23 +132,72 @@ public class MyUI extends UI {
         boutonEau.setIcon(FontAwesome.TINT);
         boutonEau.addStyleName(ValoTheme.BUTTON_ICON_ALIGN_TOP);
 
-        layout3.addComponents(boutonComparateur, boutonListePoissons, boutonEau);
-        //layout3.setMargin(true);
+        layout3.addComponents(boutonListePoissons, boutonEau);
         layout3.setSpacing(true);
         setContent(layout3);
 
+        layout4.addComponents(choixPoisson,boutonAjouter,description,compatibilite);
+        choixPoisson.setItemCaptionMode(ItemCaptionMode.ID);
+
+        choixPoisson.addValueChangeListener(event -> {
+                                            layoutPrincipal.addComponent(new Label("Le poisson 1 choisi : " + event.getProperty().getValue()));
+                                            description.setValue("Description du poisson 1 : " + event.getProperty().getValue());
+                                            compatibilite.setValue("Caractéristiques du poisson 1 : " + event.getProperty().getValue());});
+
+        layout4.setMargin(true);
+        layout4.setSpacing(true);
+        setContent(layout4);
+
+        layout5.addComponent(compatibilite);
+        layout5.setMargin(true);
+        layout5.setSpacing(true);
+        setContent(layout5);
+
+        layout6.addComponent(compatibilite2);
+        layout6.setMargin(true);
+        layout6.setSpacing(true);
+        setContent(layout6);
+
+        layout7.addComponents(choixPoisson2,boutonAjouter2,description2);
+        choixPoisson2.setItemCaptionMode(ItemCaptionMode.ID);
+        choixPoisson2.addValueChangeListener(event ->
+                                             layoutPrincipal.addComponent(new Label("Le poisson 2 choisi : " + event.getProperty().getValue())));
+        choixPoisson2.addValueChangeListener(event ->
+                                             description2.setValue("Description du poisson 2 : " + event.getProperty().getValue()));
+        choixPoisson2.addValueChangeListener(event ->
+                                             compatibilite2.setValue("Caractéristiques du poisson 2 : " + event.getProperty().getValue()));
+
+        layout7.setMargin(true);
+        layout7.setSpacing(true);
+        setContent(layout7);
+
+        layout2.addComponent(layout3,0,0);
+        layout2.addComponent(layout4,1,0);
+        layout2.addComponent(layout5,2,0);
+        layout2.addComponent(layout6,3,0);
+        layout2.addComponent(layout7,4,0);
+        layout2.setComponentAlignment(layout3, Alignment.TOP_CENTER);
+        layout2.setComponentAlignment(layout4, Alignment.TOP_CENTER);
+        layout2.setComponentAlignment(layout5, Alignment.TOP_CENTER);
+        layout2.setComponentAlignment(layout6, Alignment.TOP_CENTER);
+        layout2.setComponentAlignment(layout7, Alignment.TOP_CENTER);
+        //layout2.setMargin(true);
+        layout2.setSizeFull();
+        layout2.setSpacing(true);
+        setContent(layout2);
+
         layoutPrincipal.setMargin(true);
         layoutPrincipal.setSpacing(true);
-        layoutPrincipal.addComponents(layout1, layout2, layout3);
+        layoutPrincipal.addComponents(layout1, layout2);
         setContent(layoutPrincipal);
 
         layoutPrincipal.setComponentAlignment(layout1, Alignment.TOP_CENTER);
-        layoutPrincipal.setComponentAlignment(layout2, Alignment.BOTTOM_CENTER);
-        layoutPrincipal.setComponentAlignment(layout3, Alignment.TOP_CENTER);
-    }
+        layoutPrincipal.setComponentAlignment(layout2, Alignment.MIDDLE_CENTER);
+        //layoutPrincipal.setComponentAlignment(layout3, Alignment.TOP_CENTER);
+}
 
-    @WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
-    @VaadinServletConfiguration(ui = MyUI.class, productionMode = false)
-    public static class MyUIServlet extends VaadinServlet {
-    }
+@WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
+@VaadinServletConfiguration(ui = MyUI.class, productionMode = false)
+public static class MyUIServlet extends VaadinServlet {
+}
 }
